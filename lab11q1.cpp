05d99d313be7353bad051a63164a83aa526c5b2f
Mohit Sharma <96767237+MohitSharma2312@users.noreply.github.com>
2022-11-10 10:13:28 +0530

    updated

---
+++ lab11q1.cpp
@@ -0,0 +1,5 @@
+/* q1)write a program to create a file that stores the name of the student which  is entered by the user from the keyboard
+ q2)wap read the contents of a file print to monitor and also write to another file
+q3)wap to find the number of characters present (lenght) of file, number of words,and number of sentences present in the file.
+q4)wap to take input content from the keyboard , store to file and print ith character to jth character .
+(i and j are keyboard input where i<j)*/
